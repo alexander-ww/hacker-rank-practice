bb4ec59bf4079bf2100944b446fac616d743ac6d
alexander-ww <awittscience@gmail.com>
2021-02-07 18:03:29 -0500

    Added Java variant of left-rotation solution

---
+++ arrays/left-rotation/java/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader (System.in));
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+        int n = Integer.parseInt(firstMultipleInput[0]);
+        int d = Integer.parseInt(firstMultipleInput[1]);
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt).collect(toList());
+        List<Integer> result = Result.rotateLeft(d, arr);
+        bufferedReader.close();
+        System.out.println(result);
+    }
+}
