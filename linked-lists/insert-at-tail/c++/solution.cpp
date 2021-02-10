10a08c7a986c9df75841975b898062d8eba4b9a2
Alexander <awittscience@gmail.com>
2021-02-10 09:06:14 -0500

    Create solution.cpp

---
+++ linked-lists/insert-at-tail/c++/solution.cpp
@@ -0,0 +1,96 @@
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+
+    // create last node
+    SinglyLinkedListNode* last = new SinglyLinkedListNode(data);
+
+    // handle empty list
+    if (head == nullptr) {
+        head = last;
+    }
+
+    // handle non-empty list
+    else {
+
+        // create a current node
+        SinglyLinkedListNode* current = head;
+
+        // navigate to the last node
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+
+        // add the last node
+        current->next = last;
+    }
+
+    return head;
+}
+
+int main()
+{
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+      	SinglyLinkedListNode* llist_head = insertNodeAtTail(llist->head, llist_item);
+        llist->head = llist_head;
+    }
+
+    print_singly_linked_list(llist->head, "\n", cout);
+    cout << "\n";
+
+    free_singly_linked_list(llist->head);
+
+    return 0;
+}
